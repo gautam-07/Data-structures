5ac9e074e7fa67b9b854ba6cc735265f3251df6b
Gautam <gomzekh@gmail.com>
2020-12-30 21:51:14 +0530

    Singly Linked List

--- s-linked.cpp
+++ s-linked.cpp
@@ -1,57 +1,16 @@
-// Singly Linked List for integers input.
+/*
+*******************************************************
+✥ Title  :- Singly Linked List
+✥ Author :- Gautam Khatter 🧐
+✥ Date   :- 31 December 2020
+*******************************************************
+✥ This Data Structure is made for integer input only.
+*******************************************************
+*/
+
 
 #include <iostream>
-#include <stdio.h>
 #include <stdlib.h>
 
-class SLL
-{
-   private:
-      struct node
-      {
-         int item;
-         node *next;
-      };
-      node *start;
-   public:
-      SLL(){   start=NULL;}
-      void InsertAtLast(int);
-      void InsertAtFirst(int);
-      void DeleteLast();
-      void DeleteFirst();
-      void ViewList();
-};
+using namespace std;
 
-void SLL::InsertAtLast(int data)
-{
-   node *n;
-   n=(node*)malloc(sizeof(node));
-   n->item=data;
-   n->next=NULL;
-   if(start==NULL)
-      start=n;
-   else
-   {
-      node *temp;
-      temp=start;
-      while(temp->next!=NULL)
-      {
-         temp=temp->next;
-      }
-      temp->next=n;
-   }
-}
-
-void SLL::InsertAtFirst(int data)
-{
-   node *n;
-   n=(node*)malloc(sizeof(node));
-   n->item=data;
-   n->next=start;
-   start=n;
-}
-
-void SLL::DeleteLast()
-{
-
-}
